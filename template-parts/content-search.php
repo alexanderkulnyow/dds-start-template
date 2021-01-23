6b43671a0d856069d4f032e83e69f46251c4ed40
alexa <alexanderkulnyow@yahoo.com>
2021-01-23 14:59:01 +0300

    init

---
+++ template-parts/content-search.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template part for displaying results in search pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @packageDds_Start_Template
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php
+			dds_start_templateposted_on();
+			dds_start_templateposted_by();
+			?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php dds_start_templatepost_thumbnail(); ?>
+
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+
+	<footer class="entry-footer">
+		<?php dds_start_templateentry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
