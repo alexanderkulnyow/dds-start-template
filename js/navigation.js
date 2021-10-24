6e22d9e4295d8d23a13df31e29601e894ba3018b
alexa <alexanderkulnyow@yahoo.com>
2021-10-24 17:43:38 +0300

    fix description

--- js/navigation.js
+++ js/navigation.js
@@ -4,34 +4,34 @@
  * Handles toggling the navigation menu for small screens and enables TAB key
  * navigation support for dropdown menus.
  */
-(function () {
+(function() {
 	var container, burger, button, menu, links, i, len;
 
 	container = document.getElementById( 'site-navigation' );
 	burger = document.getElementById( 'burger' );
-	if ( !container ) {
+	if( !container ) {
 		return;
 	}
 
 	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button ) {
+	if( 'undefined' === typeof button ) {
 		return;
 	}
 
 	menu = container.getElementsByTagName( 'ul' )[0];
 
 	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
+	if( 'undefined' === typeof menu ) {
 		button.style.display = 'none';
 		return;
 	}
 
-	if ( - 1 === menu.className.indexOf( 'nav-menu' ) ) {
+	if( -1 === menu.className.indexOf( 'nav-menu' ) ) {
 		menu.className += ' nav-menu';
 	}
 
-	button.onclick = function () {
-		if ( - 1 !== container.className.indexOf( 'toggled' ) ) {
+	button.onclick = function() {
+		if( -1 !== container.className.indexOf( 'toggled' ) ) {
 			container.className = container.className.replace( ' toggled', '' );
 			burger.className = burger.className.replace( 'dashicons-no', 'dashicons-menu-alt3' );
 			button.setAttribute( 'aria-expanded', 'false' );
@@ -43,10 +43,10 @@
 	};
 
 	// Close small menu when user clicks outside
-	document.addEventListener( 'click', function (event) {
+	document.addEventListener( 'click', function(event) {
 		var isClickInside = container.contains( event.target );
 
-		if ( !isClickInside ) {
+		if( !isClickInside ) {
 			container.className = container.className.replace( ' toggled', ' ' );
 			burger.className = burger.className.replace( ' dashicons-no', ' dashicons-menu-alt3' );
 
@@ -58,7 +58,7 @@
 	links = menu.getElementsByTagName( 'a' );
 
 	// Each time a menu link is focused or blurred, toggle focus.
-	for ( i = 0, len = links.length; i < len; i ++ ) {
+	for( i = 0, len = links.length; i < len; i++ ) {
 		links[i].addEventListener( 'focus', toggleFocus, true );
 		links[i].addEventListener( 'blur', toggleFocus, true );
 	}
@@ -70,10 +70,10 @@
 		var self = this;
 
 		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while ( - 1 === self.className.indexOf( 'nav-menu' ) ) {
+		while( -1 === self.className.indexOf( 'nav-menu' ) ) {
 			// On li elements toggle the class .focus.
-			if ( 'li' === self.tagName.toLowerCase() ) {
-				if ( - 1 !== self.className.indexOf( 'focus' ) ) {
+			if( 'li' === self.tagName.toLowerCase() ) {
+				if( -1 !== self.className.indexOf( 'focus' ) ) {
 					self.className = self.className.replace( ' focus', '' );
 				} else {
 					self.className += ' focus';
@@ -87,18 +87,18 @@
 	/**
 	 * Toggles `focus` class to allow submenu access on tablets.
 	 */
-	(function () {
+	(function() {
 		var touchStartFn,
 			parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
 
-		if ( 'ontouchstart' in window ) {
-			touchStartFn = function (e) {
+		if( 'ontouchstart' in window ) {
+			touchStartFn = function(e) {
 				var menuItem = this.parentNode;
 
-				if ( !menuItem.classList.contains( 'focus' ) ) {
+				if( !menuItem.classList.contains( 'focus' ) ) {
 					e.preventDefault();
-					for ( i = 0; i < menuItem.parentNode.children.length; ++ i ) {
-						if ( menuItem === menuItem.parentNode.children[i] ) {
+					for( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+						if( menuItem === menuItem.parentNode.children[i] ) {
 							continue;
 						}
 						menuItem.parentNode.children[i].classList.remove( 'focus' );
@@ -109,7 +109,7 @@
 				}
 			};
 
-			for ( i = 0; i < parentLink.length; ++ i ) {
+			for( i = 0; i < parentLink.length; ++i ) {
 				parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
 			}
 		}
